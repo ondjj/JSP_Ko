638755340159e61edaf618d760fb725d5e0cca49
ondjj <jcjk0302@gmail.com>
2022-09-26 17:02:23 +0900

    re

---
+++ JspChap03/src/main/java/LoginServlet.java
@@ -0,0 +1,41 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet({ "/LoginServlet", "/Login" })
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		response.setCharacterEncoding("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("아이디="+id+"<br>");
+		out.println("패스워드="+password+"<br>");
+		
+	}
+
+
+}
